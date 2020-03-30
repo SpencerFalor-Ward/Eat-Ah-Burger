24c7c3efba6b75679b949c804f58eead82620e12
SpencerFW <srfalorward@ucdavis.edu>
2020-03-30 10:38:43 -0700

    gitignore and sql files started

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) values ('buffalo blue', flase);
+INSERT INTO burgers (burger_name, devoured) values ('lamb', true);
+INSERT INTO burgers (burger_name, devoured) values ('Jalapeno', true);
