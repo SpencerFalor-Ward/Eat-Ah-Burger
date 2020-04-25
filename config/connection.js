e81292451e1fc7d265fcc80ef02b38b21ace9580
SpencerFalor-Ward <srfalorward@ucdavis.edu>
2020-04-25 11:39:20 -0700

    fixed connection file

--- config/connection.js
+++ config/connection.js
@@ -1,36 +1,20 @@
 // Set up MySQL connection.
-var mysql = require('mysql');
-// var conneection = require('dotenv').config();
-var connection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'burgers_db',
-})
-if(process.env.JAWSBD_URL){
-    connection = mysql.createConnection(process.env.JAWSBD_URL);}else{
-connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password:'root',
-    database: 'burgers_db'
-})
-//     }
-// var mysql = require('mysql')
+var mysql = require('mysql')
+var connection
 
-// var connection
-// if (process.env.JAWSDB_URL) {
-//     // Database is JawsDB on Heroku
-//     // connection = mysql.createConnection(process.env.JAWSDB_URL)
-// } else {
-//     // Database is local
-//     connection = mysql.createConnection(process.env.pw)
-// }
-
-// var mysql = require('mysql')
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+        database: 'burgers_db',
+    })
+}
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack)
         return
